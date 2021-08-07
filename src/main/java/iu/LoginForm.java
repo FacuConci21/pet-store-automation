e502831ffaa9b1d50524cbf48152bff5f1a23343
Nelson Campos <nelsoncamposcapa@gmail.com>
2021-08-07 14:32:16 -0300

    Hot Fix

--- src/main/java/iu/LoginForm.java
+++ src/main/java/iu/LoginForm.java
@@ -5,7 +5,7 @@ import net.serenitybdd.screenplay.targets.Target;
 public interface LoginForm {
 
     Target INPUT_TXT_USERNAME = Target.the("Username text box")
-            .locatedBy("//input[@id='stripes--1981077719']");
+            .locatedBy("//input[@name='username']");
     Target INPUT_TXT_PASSWORD = Target.the("Password text box")
             .locatedBy("//input[@name='password']");
     Target INPUT_SUBMIT_LOGIN = Target.the("Login button")
